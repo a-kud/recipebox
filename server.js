bfd622d3f300ce0eab2ac1b64f3e3513764705a6
Andrew Kud <kud@mm.st>
2017-03-13 12:07:37 +0200

    add express

---
+++ server.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var express = require('express');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var app = express();
+var compiler = webpack(webpackConfig);
+var webpackHotMiddleware = require("webpack-hot-middleware");
+
+
+app.set("views", __dirname + "/src/views");
+app.set("view engine", "pug");
+
+app.get("/", function (req, res) {
+  res.render("index");
+})
+
+
+app.use(webpackDevMiddleware(compiler, {
+    hot: true,
+    filename: "bundle.js",
+    publicPath: "/build/",
+    stats: {
+        colors: true,
+    },
+    historyApiFallback: true,
+}));
+app.use(webpackHotMiddleware(compiler, {
+    log: console.log,
+    path: "/__webpack_hmr",
+    heartbeat: 10 * 1000,
+}));
+
+app.use(express.static(__dirname + "/build"));
+
+var server = app.listen(3004, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(`Recipebox listening at http://${host}:${port}`);
+});
