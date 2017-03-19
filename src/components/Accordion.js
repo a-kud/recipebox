6b6b12ec3742cafca63d68a876a7a6fe8ce42a0b
Andrew Kud <kud@mm.st>
2017-03-19 14:57:25 +0200

    add collapsible

---
+++ src/components/Accordion.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Accordion extends React.Component {
+
+    return (
+        <button className="accordion"></button>
+        <div className="panel"></div>
+    );
+}
