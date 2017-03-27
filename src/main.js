f5df81a0027b94495afe1b81b7574140d93c962c
Andrew Kud <kud@mm.st>
2017-03-27 19:39:39 +0300

    add sass

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
+require("./sass/main.sass");
 
 const container = document.querySelector(".container");
 
