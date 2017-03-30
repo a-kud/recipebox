4ae30ccb47ffa72199f3d5cdb2a834fd27a1fafd
Andrew Kud <kud@mm.st>
2017-03-30 20:06:28 +0300

    add panel toggle

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,11 +2,23 @@ import React from "react";
 
 class Accordion extends React.Component {
 
+    handleClick(e) {
+        let panel = e.target.nextElementSibling;
+        console.log(panel.scrollHeight);
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+
     render() {
         return (
             <div>
-                <button className="accordion"></button>
-                <div className="panel"></div>
+                <button className="accordion" onClick={(e) => this.handleClick(e)}></button>
+                <div className="panel">
+                    <p>sdfdfsgsgfhjjk</p>
+                </div>
             </div>
         );
     };
