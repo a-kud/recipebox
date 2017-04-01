4f0e0399cb6ea0e6433427684aafa64ccdde3a43
Andrew Kud <kud@mm.st>
2017-04-01 23:16:44 +0300

    add button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Accordion from "./Accordion";
+import AddButton from "./buttons/AddButton";
 
 class RecipeBox extends React.Component {
     render() {
         return (
-            <div className="background">
-                <Accordion />
+            <div>
+                <div className="background"></div>
+                <AddButton />
             </div>
         );
     };
---
+++ src/components/buttons/AddButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AddButton extends React.Component {
+
+    render() {
+        return (
+            <button className="btn-add">
+                Add Recipe
+            </button>
+        )
+    }
+}
+
+export default AddButton;
