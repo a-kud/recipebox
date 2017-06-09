b9ac2846a969782d1279c412e1a647e1009c5ebb
Andrew Kud <kud@mm.st>
2017-06-09 19:35:51 +0300

    add recipe delete feature

--- src/components/DeleteRecipe.js
+++ src/components/DeleteRecipe.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 class DeleteRecipe extends React.Component {
-    
+
+    handleClick() {
+        
+    }
+
+    render() {
+        return <button onClick={this.handleClick()}>Delete</button>
+    }
 }
