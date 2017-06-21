3d5254389593ca6e8966912b45489187ef5c4c2c
Andrew Kud <kud@mm.st>
2017-06-21 23:05:42 +0300

    add edit feature

--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -1,13 +1,39 @@
 import React from "react";
+import RecipeForm from "./RecipeForm";
 
+import {ModalContainer, ModalDialog} from "react-modal-dialog";
 
 class EditRecipe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isShowingModal: false,
+      };
 
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClick() {
+        this.setState({isShowingModal: true});
+    }
+
+    handleClose() {
+        this.setState({isShowingModal: false});
+    }
     render() {
         return (
             <div>
-            <button onClick={() => alert("Click")}>Edit</button>
-
+            <button onClick={this.handleClick}>Edit</button>
+            {
+              this.state.isShowingModal &&
+              <ModalContainer onClose={() => this.handleClose() }>
+                <ModalDialog onClose={ () => this.handleClose() }>
+                  <RecipeForm recipe={this.props.recipe}
+                              handleFormSubmit={this.handleRecipeSubmit}/>
+                </ModalDialog>
+              </ModalContainer>
+            }
             </div>
         );
     }
