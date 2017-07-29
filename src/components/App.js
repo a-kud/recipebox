6f537a8bdf87182e78dadc26be9302f4f09673d5
Andrew Kud <kud@mm.st>
2017-07-30 02:27:12 +0300

    Fix one ingridient entered for edit

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class RecipeBox extends React.Component {
             let id = args[0]; // recipe id
             let index = recipes.map( (recipe) => recipe.id ).indexOf(id); // find index of recipe by its id
             recipes[index].name = args[1].editedName; // args[1] - edited recipe data
-            recipes[index].ingridients = args[1].editedIngridients.split(",");
+            recipes[index].ingridients = args[1].editedIngridients.length == 1 ? args[1].editedIngridients : args[1].editedIngridients.split(",");
         } else {
             recipes.push({
                 name: this.state.currentName,
